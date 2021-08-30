60654e48df528d3a4a7f5810d2f653c373379d15
cryptovikings-thor <84129320+cryptovikings-thor@users.noreply.github.com>
2021-08-30 19:27:56 +0100

    basic discord.js client implementation

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -6,6 +6,6 @@ import dotenvsafe from 'dotenv-safe';
  * Module designed to be passed directly to Node for pre-execution inclusion with the `-r` flag
  */
 dotenvsafe.config({
-    path: `${__dirname}/../.env`,
-    example: `${__dirname}/../.env.example`
+    path: `${__dirname}/../../.env`,
+    example: `${__dirname}/../../.env.example`
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log('hi');
+import { Client, Intents } from 'discord.js';
+
+if (!process.env.DISCORD_BOT_TOKEN) {
+    console.error('No Discord Bot Token Found in Environment');
+    process.exit(1);
+}
+
+if (!process.env.DISCORD_RECENT_VIKINGS_CHANNEL_ID) {
+    console.error('No Recent Vikings Channel ID Found in Environment');
+    process.exit(1);
+}
+
+if (!process.env.DISCORD_TEST_CHANNEL_ID) {
+    console.error('No Test Channel ID Found in Environment');
+    process.exit(1);
+}
+
+// bot client
+const client = new Client({
+    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
+});
+
+client.on('messageCreate', async (message) => {
+    if (process.env.DEV === 'true' && message.channel.id !== process.env.DISCORD_TEST_CHANNEL_ID) {
+        return;
+    }
+
+    if (client.user === message.author) {
+        return;
+    }
+
+    if (message.content === '!goodbot') {
+        await message.reply(':blush: :blush:');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN!).then(
+    () => {
+        console.log('INITIALIZED');
+    },
+    (err) => {
+        console.error('ERROR:', err);
+    }
+);
