f273ba3b313d234fed91272c408c17fc24d90788
cryptovikings-thor <84129320+cryptovikings-thor@users.noreply.github.com>
2021-08-31 15:47:55 +0100

    document Recent Vikings Module

--- src/bot/modules/recent-vikings.ts
+++ src/bot/modules/recent-vikings.ts
@@ -10,23 +10,29 @@ import path from 'path/posix';
  */
 export class RecentVikings {
 
+    /** Contract address */
     private readonly contractAddress = process.env.ETH_CONTRACT_ADDRESS!;
 
+    /** RPC Provider URL */
     private readonly provider = new providers.JsonRpcProvider(process.env.ETH_PROVIDER_URL);
 
+    /** Contract */
     private readonly contract = new Contract(this.contractAddress, nornirABI, this.provider);
 
+    /** Polling Interval for Contract Event listening */
     private readonly pollingInterval = parseInt(process.env.ETH_LISTEN_INTERVAL!, 10);
 
     /** Recent Vikings Channel ID to post messages to */
     private recentVikingsChannelId = process.env.MODULE_RECENT_VIKINGS_CHANNEL_ID!;
 
+    /** Instantiated TextChannel */
     private channel: TextChannel | undefined;
 
+    /** API Viking Output directory to serve as input for messages */
     private readonly vikingImageDirectory = path.join(__dirname, '../../../../', process.env.MODULE_RECENT_VIKINGS_IMAGE_INPUT!);
 
     /**
-     * Constructor
+     * Constructor - retrieve the channel and set up the Contract event listener
      *
      * @param client the Discord.js Client
      */
@@ -50,6 +56,11 @@ export class RecentVikings {
         );
     }
 
+    /**
+     * Contract VikingComplete event handler - send a message to the feed channel
+     *
+     * @param id
+     */
     private onVikingComplete(id: BigNumber): void {
         void this.channel?.send({
             content: this.message(id.toNumber()),
@@ -59,12 +70,26 @@ export class RecentVikings {
         });
     }
 
+    /**
+     * Message constructor for a given Viking ID
+     *
+     * @param id the ID of the Viking
+     *
+     * @returns the message
+     */
     private message = (id: number): string => `
     **New CryptoViking!**
 
 Viking #${id} has just been minted! Check him out: https://cryptovikings.io/vikings/${id}
 `;
 
+    /**
+     * Type Guard for differentiating TextChannels from Channels
+     *
+     * @param channel the channel to check
+     *
+     * @returns whether or not the channel is a TextChannel
+     */
     private isTextChannel(channel: Channel | TextChannel): channel is TextChannel {
         return !!(channel as TextChannel).send;
     }
