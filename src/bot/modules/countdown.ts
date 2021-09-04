3e95a9f1be1460f3c53ebd100678812631a1c78d
cryptovikings-thor <84129320+cryptovikings-thor@users.noreply.github.com>
2021-09-04 16:46:27 +0100

    [WIP] automated accelerating Countdown posting based on proximity to launch

--- src/bot/modules/countdown.ts
+++ src/bot/modules/countdown.ts
@@ -1,5 +1,6 @@
 import { Client, TextChannel } from 'discord.js';
 import { ChannelUtils } from '../../utils/channel';
+import { TimeUtils } from '../../utils/time';
 
 /**
  * Countdown module - regularly posts a launch countdown to a specified channel
@@ -33,15 +34,55 @@ export class Countdown {
     }
 
     private start(): void {
-        let time = 1000;
+        const onTimeout = (): void => {
+            this.onInterval('interval');
 
-        const interval = (): void => {
-            this.onInterval(`timeout: ${time}`);
-            time += 1000;
-            setTimeout(interval, time);
+            const timeout = this.getTimeout();
+
+            if (timeout) {
+                this.onInterval(`timeout - ${timeout}`)
+
+                setTimeout(onTimeout, timeout);
+            }
         }
 
-        setTimeout(interval, time);
+        const timeout = this.getTimeout();
+
+        if (timeout) {
+            this.onInterval(`first timeout - ${timeout}`);
+            setTimeout(onTimeout, timeout);
+        }
+    }
+
+    private getTimeout(): number | undefined {
+        const countdown = TimeUtils.timeToLaunch(Date.now());
+        let timeout;
+
+        this.onInterval(JSON.stringify(countdown));
+
+        if (countdown.hasPassed) {
+            // exit case for either not starting the interval or for cancelling it when launch has passed
+            return;
+        }
+
+        if (countdown.weeks > 0 || countdown.days > 0) {
+            // more than a day -> post daily
+            timeout = 1000 * 60 * 60 * 24;
+        }
+        else if (countdown.hours > 1) {
+            // more than an hour -> post hourly
+            timeout = 1000 * 60 * 60;
+        }
+        else if (countdown.minutes > 15) {
+            // more than 15 minutes -> post every 15 minutes
+            timeout = 1000 * 60 * 15;
+        }
+        else {
+            // post every minute
+            timeout = 1000 * 60;
+        }
+
+        return timeout;
     }
 
     private onInterval(message: string): void {
