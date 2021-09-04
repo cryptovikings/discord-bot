d825cbb228b3637a09427aeec5b825df31178c81
cryptovikings-thor <84129320+cryptovikings-thor@users.noreply.github.com>
2021-09-04 16:08:34 +0100

    basis for an adaptable interval for automatic Countdown posting

--- src/bot/modules/countdown.ts
+++ src/bot/modules/countdown.ts
@@ -6,6 +6,8 @@ import { ChannelUtils } from '../../utils/channel';
  */
 export class Countdown {
 
+    private countdownStart = parseInt(process.env.MODULE_COUNTDOWN_START!, 10);
+
     /** Countdown Channel ID to post messages to */
     private countdownChannelId = process.env.MODULE_COUNTDOWN_CHANNEL_ID!;
 
@@ -18,7 +20,7 @@ export class Countdown {
                 if (channel && ChannelUtils.isTextChannel(channel)) {
                     this.channel = channel;
 
-                    // setInterval(this.onInterval.bind(this), 1000);
+                    this.start();
                 }
                 else {
                     console.error('Channel is not a TextChannel');
@@ -30,7 +32,19 @@ export class Countdown {
         );
     }
 
-    private onInterval(): void {
-        void this.channel?.send('test');
+    private start(): void {
+        let time = 1000;
+
+        const interval = (): void => {
+            this.onInterval(`timeout: ${time}`);
+            time += 1000;
+            setTimeout(interval, time);
+        }
+
+        setTimeout(interval, time);
+    }
+
+    private onInterval(message: string): void {
+        void this.channel?.send(`test - ${message}`);
     }
 }
